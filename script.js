89dae90c6bfcccc281f36ffafc8e49540c278905
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-04-03 12:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-.images-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.images-container img {
-  margin: 10px;
-  cursor: pointer;
-  transition: transform 0.2s;
-}
-
-.images-container img.selected {
-  transform: scale(1.1);
+function letterCombinations(input_digit) {
+  //Complete the function
+	   
+    if(input_digit == ""){
+        return [];
+    }
+    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+     
+    let res =[];
+    let que = [''];
+     
+    while(que.length>0){
+        let str = que[0];
+        que.shift();
+         
+        if(str.length == input_digit.length){
+            res.push(str); // if all digits are replaced with char push to result
+        } else{
+//             get the current number from the digits i.e if str.length = 2 , digits =123 s= 3
+            let s= Number(input_digit.charAt(str.length));
+            let value = table[s]; 
+			
+             
+            for(i=0;i<value.length;i++){
+                que.push(str+value.charAt(i));
+            }
+        }
+    }
+     
+    return res;
+     
 }
