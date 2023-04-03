b2a93a699dd792055a79d0b230f5b202c610e6f6
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-04-03 12:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-	   
-    if(input_digit == ""){
-        return [];
-    }
-    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
-     
-    let res =[];
-    let que = [''];
-     
-    while(que.length>0){
-        let str = que[0];
-        que.shift();
-         
-        if(str.length == input_digit.length){
-            res.push(str); // if all digits are replaced with char push to result
-        } else{
-//             get the current number from the digits i.e if str.length = 2 , digits =123 s= 3
-            let s= Number(input_digit.charAt(str.length));
-            let value = table[s]; 
-			
-             
-            for(i=0;i<value.length;i++){
-                que.push(str+value.charAt(i));
-            }
-        }
-    }
-     
-    return res;
-     
+function letterCombinations(digits) {
+if (!digits) {
+return [];
 }
+
+const mappings = {
+'2': 'abc',
+'3': 'def',
+'4': 'ghi',
+'5': 'jkl',
+'6': 'mno',
+'7': 'pqrs',
+'8': 'tuv',
+'9': 'wxyz'
+};
+
+const results = [];
+
+function generateCombinations(currentIndex, currentString) {
+if (currentIndex === digits.length) {
+results.push(currentString);
+return;
+}
+
+const currentDigit = digits[currentIndex];
+const letters = mappings[currentDigit];
+
+for (let i = 0; i < letters.length; i++) {
+const letter = letters[i];
+generateCombinations(currentIndex + 1, currentString + letter);
+}
+}
+
+generateCombinations(0, '');
+
+return results.sort();
+}
+module.exports = letterCombinations;
